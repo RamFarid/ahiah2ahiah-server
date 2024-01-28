5a7458bc7228a5dd90c3950ae16690cec3422c36
RamFarid <workprojects22@gmail.com>
2024-01-28 21:23:39 +0200

    Add the cclient origin

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose
 
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://ahiah2ahiah.vercel.app'],
   })
 )
 app.use(bodyParser.json())
